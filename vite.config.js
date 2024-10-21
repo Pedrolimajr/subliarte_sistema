2d34377dc8243f4f8372f591be67e7748dd8e983
Pedrolimajr <pedrosouzalima@hotmail.com>
2024-10-21 20:29:09 -0300

    Ajuste no main.jsx

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/subliarte_sistema/', // Nome do seu repositório
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',  // Certifique-se de que o caminho está correto
